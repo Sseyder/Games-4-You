49c5f0ad6c9044b7d2a730bb66605f3b5ec89be4
Leonel <leonel.marsilli.214@gmail.com>
2022-01-22 16:07:56 -0300

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 import Slider from '../components/Slider';
+import ItemCount from '../components/ItemCount/ItemCount';
 
 function Home() {
   return <div>
       <Navbar/>
       <Announcement/>
       <Slider/>
+      <ItemCount/>
   </div>;
 }
 
