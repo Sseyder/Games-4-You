9c691bcfd5ff9a511bf5a63e67399ef7d13e89c8
Leonel <leonel.marsilli.214@gmail.com>
2022-01-22 14:27:34 -0300

    commity

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,7 +6,6 @@ const Container = styled.div`
   display: flex;
   padding: 20px;
   justify-content: space-between;
-  ${mobile({ padding: "0px", flexDirection:"column" })}
 `;
 
 const Categories = () => {
