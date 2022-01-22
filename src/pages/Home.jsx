9d9667f640acb0b5e0cf0825c395d49ee1436f7f
Leonel <leonel.marsilli.214@gmail.com>
2022-01-22 15:07:37 -0300

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 import Slider from '../components/Slider';
-import Categories from '../components/Categories';
-import Products from '../components/Products';
 
 function Home() {
   return <div>
-      <Announcement />
       <Navbar />
+      <Announcement />
       <Slider />
-      <Categories />
-      <Products /> 
   </div>;
 }
 
